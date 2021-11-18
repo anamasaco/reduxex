0300c105979eba031808c964380131fcd2da1066
AnaMasaco <ana.masaco@gmail.com>
2021-11-18 11:41:21 +0000

    new branch and changes

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import Characters from './components/Characters';
+import { Characters } from './components/Characters';
 import './App.css';
-
-/* BASE EXERCICIO:
-mostrar 10 nomes na primeira busca
-qdo clico num nome dá os restantes dados
-se clico num outro nome dá os dados dessa pessoa
-e a anterior desaparece
-
-API - https://swapi.dev/api/people
-*/
-
 
 function App() {
   return (
